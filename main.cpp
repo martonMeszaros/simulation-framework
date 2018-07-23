51e44cc55253bb2dcd6acb71b6575e327065dcc4
Márton Mészáros <marton.m95@gmail.com>
2018-07-23 23:01:56 +0200

    Update example in main

--- main.cpp
+++ main.cpp
@@ -8,18 +8,25 @@ int main() {
     Initializer initializer;
     Window window("Hello World!", 480, 480, false);
     Renderer renderer(window, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-    const RGBA PURPLE(255, 0, 255);
     const RGBA WHITE(255, 255, 255);
-    const Rect rect(0, 0, 100, 215);
-    renderer.fill();
-    renderer.present();
+    const RGBA PURPLE(128, 0, 255);
+    Rect rect(0, 0, 240, 240);
 
     using namespace std::this_thread;
     using namespace std::chrono;
-    sleep_for(seconds(1));
-    renderer.setDrawColor(WHITE);
-    renderer.fill();
-    renderer.present();
-    sleep_for(seconds(1));
+    for (int i = 0; i < 2; ++i) {
+        for (int y = 0; y < 2; ++y) {
+            for (int x = 0; x < 2; ++x) {
+                rect.x = x * 240;
+                rect.y = y * 240;
+                renderer.setDrawColor(WHITE);
+                renderer.clear();
+                renderer.setDrawColor(PURPLE);
+                renderer.fillRect(rect);
+                renderer.present();
+                sleep_for(milliseconds(500));
+            }
+        }
+    }
     return 0;
 }
