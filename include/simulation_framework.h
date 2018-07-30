4eb9bd75b5c76f05200ad626f8ed769c36e396bc
Márton Mészáros <marton.m95@gmail.com>
2018-07-30 12:22:19 +0200

    simulation_framework header file

---
+++ include/simulation_framework.h
@@ -0,0 +1,13 @@
+#ifndef SIMULATION_FRAMEWORK_SIMULATION_FRAMEWORK_H
+#define SIMULATION_FRAMEWORK_SIMULATION_FRAMEWORK_H
+
+#include "enums/EventType.h"
+#include "enums/RendererFlags.h"
+#include "enums/SubSystems.h"
+#include "enums/WindowFlags.h"
+#include "Initializer.h"
+#include "Simulation.h"
+
+namespace sf {}
+
+#endif //SIMULATION_FRAMEWORK_SIMULATION_FRAMEWORK_H
